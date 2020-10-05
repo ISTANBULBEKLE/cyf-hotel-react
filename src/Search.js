dbc8a56896deace0ffa8b5764be147d26dda6096
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-05 16:55:14 +0100

    lesson 2-18 is done

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchButton from "./SearchButton";
 
-const Search = () => {
+const Search = ({ search }) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  function handleSearchInput(e) {
+    console.log(e.target.value);
+    setSearchInput(e.target.value);
+  }
+  const [submission, setSubmission] = useState("");
+
+  function handleSubmission(e) {
+    e.preventDefault();
+    search(searchInput);
+  }
   return (
     <div className="search">
       <div className="page-header">
@@ -9,12 +21,14 @@ const Search = () => {
       </div>
       <div className="row search-wrapper">
         <div className="col">
-          <form className="form-group search-box">
+          <form className="form-group search-box" onSubmit={handleSubmission}>
             <label htmlFor="customerName">Customer name</label>
             <div className="search-row">
               <input
                 type="text"
                 id="customerName"
+                value={searchInput}
+                onChange={handleSearchInput}
                 className="form-control"
                 placeholder="Customer name"
               />
